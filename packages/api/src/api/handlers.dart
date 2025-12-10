631a64a3a0a24126127c2eef6ac1a58d54878bc4
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-12-10 17:12:39 +0100

    Stop streaming data after web sockets close (closes RR-37)

---
+++ packages/api/src/api/handlers.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+extension RouterHandlers on Router {
+  void getJson(String route, JsonRequestHandler handler) {
+    get(route, handler);
+  }
+
+  void postJson(String route, JsonRequestHandler handler) {
+    post(route, handler);
+  }
+
+  void mountWs(String prefix, WebSocketHandler handler) {
+    mount(prefix, webSocketHandler((webSocket, _) => handler(webSocket)));
+  }
+}
+
+extension SinkHandlers on Sink {
+  void addJson(Map<String, dynamic> json) {
+    add(jsonEncode(json));
+  }
+}
+
+class JsonResponse extends Response {
+  JsonResponse.ok([Map<String, dynamic>? json]) : super.ok(json != null ? jsonEncode(json) : null);
+  JsonResponse.noContent() : super(204);
+}
+
+typedef JsonRequestHandler = Future<JsonResponse> Function(Request request);
+typedef WebSocketHandler = void Function(WebSocketChannel webSocket);
+
+enum WebSocketStatus { open, closed }
+
+extension WebSocketChannelStatus on WebSocketChannel {
+  WebSocketStatus get status => closeCode == null ? .open : .closed;
+}
--- packages/api/src/api/router.dart
+++ packages/api/src/api/router.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
 import 'package:remote_rift_connector_core/remote_rift_connector_core.dart';
-import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
-import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 import 'api.dart';
+import 'handlers.dart';
 
 extension RemoteRiftApiRouter on RemoteRiftApi {
   Router configureRouter() {
@@ -21,9 +18,10 @@ extension on Router {
   void configureStatus() {
     String route(String value) => '/status/$value';
 
-    mountWs(route('watch'), (_, outbound) async {
+    mountWs(route('watch'), (webSocket) async {
       await for (var status in RemoteRiftConnector().getStatusStream()) {
-        outbound.addJson(status.sealedToJson());
+        if (webSocket.status == .closed) break;
+        webSocket.sink.addJson(status.sealedToJson());
       }
     });
   }
@@ -36,9 +34,10 @@ extension on Router {
       return .ok(state.sealedToJson());
     });
 
-    mountWs(route('watch'), (_, outbound) async {
+    mountWs(route('watch'), (webSocket) async {
       await for (var state in RemoteRiftConnector().getCurrentSateStream()) {
-        outbound.addJson(state.sealedToJson());
+        if (webSocket.status == .closed) break;
+        webSocket.sink.addJson(state.sealedToJson());
       }
     });
   }
@@ -79,33 +78,5 @@ extension on Router {
       await RemoteRiftConnector().declineMatch();
       return .noContent();
     });
-  }
-}
-
-extension on Router {
-  void getJson(String route, RequestHandler handler) {
-    get(route, handler);
-  }
-
-  void postJson(String route, RequestHandler handler) {
-    post(route, handler);
-  }
-
-  void mountWs(String prefix, WebSocketHandler handler) {
-    mount(prefix, webSocketHandler((webSocket, _) => handler(webSocket.stream, webSocket.sink)));
-  }
-}
-
-typedef RequestHandler = Future<JsonResponse> Function(Request request);
-typedef WebSocketHandler = void Function(Stream inbound, Sink outbound);
-
-class JsonResponse extends Response {
-  JsonResponse.ok(Map<String, dynamic> json) : super.ok(jsonEncode(json));
-  JsonResponse.noContent() : super(204);
-}
-
-extension on Sink {
-  void addJson(Map<String, dynamic> json) {
-    add(jsonEncode(json));
   }
 }
